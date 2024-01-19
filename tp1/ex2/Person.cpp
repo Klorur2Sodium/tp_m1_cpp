964ff7f4dec8ab7f6defaa5e610f315759cc66b5
FRAIZE Victor <vfraize3@gmail.com>
2024-01-19 11:46:49 +0100

    td1 - ex2 - q4

---
+++ tp1/ex2/Person.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include "Person.hpp"
+
+
+Person::Person(std::string first_name, std::string surname)
+    : first_name { first_name }
+    , surname { surname } {}
+
+std::string Person::get_first_name() {
+        return first_name;
+    }
+
+std::string Person::get_surname() {
+        return surname;
+    }
--- tp1/ex2/Person.hpp
+++ tp1/ex2/Person.hpp
@@ -1,8 +1,14 @@
 #include <string>
 
-class Person
-{
-public:
+class Person {
+private:
     std::string first_name;
     std::string surname;
+    
+public:
+    Person(std::string first_name, std::string surname);
+
+    std::string get_first_name();
+
+    std::string get_surname();
 };
