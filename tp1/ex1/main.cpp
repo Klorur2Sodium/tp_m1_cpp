bb8de677b1e3fad050ad6252474b454fdac62f7a
FRAIZE Victor <vfraize3@gmail.com>
2024-01-19 11:12:54 +0100

    td1 - ex1 - q6

--- tp1/ex1/main.cpp
+++ tp1/ex1/main.cpp
@@ -1,13 +1,17 @@
 #include <iomanip>
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::cout << "Entre ton nom: ";
 
-    char name[20] = "";
-    std::cin >> std::setw(20) >> name;
+    std::string name = "";
+    std::cin >> name;
     std::cout << "Bonjour " << name << std::endl;
-    
+
     return 0;
 }
+
+// q5 - Si on donne une chaine de caractère d'une longueur de 21 ou plus,
+//      tous les caractères au delà du 20eme sont coupés.
